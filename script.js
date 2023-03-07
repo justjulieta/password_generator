af8ca3896bf52713aaab09204a138adacd69ff00
Julieta Ramirez Solis Nguyen <julieta.rammirez27@gmail.com>
2023-03-06 19:03:46 -0800

    Changes to be committed:

--- script.js
+++ script.js
@@ -8,13 +8,16 @@ var specialCharacters = ['!','@','#','$','%','^','&','*','(',')','_','-','+','='
 var numbers = ['0','1','2','3','4','5','6','7','8','9'];
 
 var userSelection = [];
+var characterLength;
+var password = '';
 
 // Event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function questions() {
   userSelection = [];
-  characterLength = parseInt(question("How many characters would you like the password to have? (Pick between 8-128 characters)")); 
+  password = '';
+  characterLength = parseInt(prompt("How many characters would you like the password to have? (Pick between 8-128 characters)")); 
 
   if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
     alert("Password must be between 8-128 characters.");
@@ -23,21 +26,27 @@ function questions() {
 
   if(confirm("Would you like uppercase letters password? (Yes or No?)")) {
     userSelection = userSelection.concat(upperCase);
+    password = password + upperCase[Math.floor(Math.random() * upperCase.length)]
   }
 
   if(confirm("Would you like lowercase letters in password? (Yes or No?)")) {
     userSelection = userSelection.concat(lowerCase);
+    password = password + lowerCase[Math.floor(Math.random() * lowerCase.length)]
   }
 
   if(confirm("Would you like special characters password? (Yes or No?)")) {
     userSelection = userSelection.concat(specialCharacters);
+    password = password + specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
   }
 
   if(confirm("Would you like numbers password? (Yes or No?)")) {
     userSelection = userSelection.concat(numbers);
+    password = password + numbers[Math.floor(Math.random() * numbers.length)]
   }
+  console.log(userSelection);
 
-  if (userSelection.character === 0) { 
+
+  if (userSelection.length === 0) { 
     console.log("It's empty!") 
     alert ("Please try again.");
     return false;
@@ -49,17 +58,24 @@ function questions() {
 }
 
 function generatePassword() {
-  var password = "";
-  for (var i = 0; i < characterLength; i++) {
-    var randomIndex = Math.floor(Math.random() * userSelection.characterLength);
+  // var password = "";
+  console.log(password)
+  console.log(password.length)
+
+  //0 --> 8
+  for (var i = password.length; i < characterLength; i++) {
+    console.log(i)
+    var randomIndex = Math.floor(Math.random() * userSelection.length);
     password = password + userSelection[randomIndex];
   }
+
   return password;
 }
 
 // Write password to the #password input
 function writePassword() {
   var answers = questions(); 
+  console.log(answers)
   var passwordText = document.querySelector("#password");
 
   if (answers) {
